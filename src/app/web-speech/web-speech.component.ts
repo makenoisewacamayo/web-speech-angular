08e7d01b5b534108445aa3578c9e3002f0708e49
Rafael Avaria G <rafael.avaria@gmail.com>
2019-11-27 17:45:16 -0300

    make some ammendant to web speech

--- src/app/web-speech/web-speech.component.ts
+++ src/app/web-speech/web-speech.component.ts
@@ -13,9 +13,10 @@ import { ActionContext } from './shared/model/strategy/action-context';
 export class WebSpeechComponent implements OnInit {
 
   finalTranscript = '';
+  nombreTranscript = '';
   recognizing = false;
   notification: string;
-  languages: string[] =  ['en-US', 'es-ES'];
+  languages: string[] =  ['en-US', 'es-ES', 'es-CL'];
   currentLanguage: string;
   actionContext: ActionContext = new ActionContext();
 
@@ -23,7 +24,7 @@ export class WebSpeechComponent implements OnInit {
               private speechRecognizer: SpeechRecognizerService) { }
 
   ngOnInit() {
-    this.currentLanguage = this.languages[0];
+    this.currentLanguage = this.languages[2];
     this.speechRecognizer.initialize(this.currentLanguage);
     this.initRecognition();
     this.notification = null;
@@ -47,7 +48,7 @@ export class WebSpeechComponent implements OnInit {
     this.speechRecognizer.onStart()
       .subscribe(data => {
         this.recognizing = true;
-        this.notification = 'I\'m listening...';
+        this.notification = 'Escucahndo, empieza a hablar...';
         this.detectChanges();
       });
 
@@ -74,15 +75,14 @@ export class WebSpeechComponent implements OnInit {
         switch (data.error) {
           case SpeechError.BLOCKED:
           case SpeechError.NOT_ALLOWED:
-            this.notification = `Cannot run the demo.
-            Your browser is not authorized to access your microphone. Verify that your browser has access to your microphone and try again.
-            `;
+            this.notification = `No se puede correr el demo.
+            Debes dar autorización para acceder a tu micrófono`;
             break;
           case SpeechError.NO_SPEECH:
-            this.notification = `No speech has been detected. Please try again.`;
+            this.notification = `El api de reconocimiento de voz no esta actica `;
             break;
           case SpeechError.NO_MICROPHONE:
-            this.notification = `Microphone is not available. Plese verify the connection of your microphone and try again.`;
+            this.notification = `El micrófono no esta disponible`;
             break;
           default:
             this.notification = null;
